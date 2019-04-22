553fb4b9f0b682ed30603f0cea048671db8fbc5c
wjdda <wj.dda.pzII.ausf.g@gmail.com>
2019-04-22 22:51:13 +0300

    Added Point2D and Point3D.

---
+++ src/com/company/gamegeom/Point2D.java
@@ -0,0 +1,29 @@
+package com.company.gamegeom;
+
+// FIXME final!
+// TODO operation +, -, ==, >= ...
+public class Point2D <T> {
+    /**
+     * 2D Point in Game
+     */
+
+    public final T x;
+    public final T y;
+
+    public Point2D (T x, T y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point2D () {
+        this(null, null);
+    }
+
+    public Point2D (Point2D<T> p) {
+        this(p.x, p.y);
+    }
+
+    public String show() {
+        return "(" + x + ", " + y + ")";
+    }
+}
---
+++ src/com/company/gamegeom/Point3D.java
@@ -0,0 +1,41 @@
+package com.company.gamegeom;
+
+import java.awt.*;
+
+// FIXME final!
+// TODO operation +, -, ==, >= ...
+public class Point3D <T> {
+    /**
+     * 3D Point in Game
+     */
+
+    public final T x;
+    public final T y;
+    public final T z;
+
+    public Point3D (T x, T y, T z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public Point3D () {
+        this(null, null, null);
+    }
+
+    public Point3D (Point3D<T> p) {
+        this(p.x, p.y, p.z);
+    }
+
+    public Point2D<T> to2D() {
+        return new Point2D<T>(this.x, this.y);
+    }
+
+    public Point toJPoint () {
+        return new Point((int) this.x, (int) this.y);
+    }
+
+    public String show() {
+        return "(" + this.x + ", " + this.y + ", " + this.z + ")";
+    }
+}
